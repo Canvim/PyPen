9809d6deaacef0319b88d2959d6bac6e0d26da1f
EriKWDev Josefins Dator <ErikWDev@gmail.com>
2020-07-10 22:12:25 +0200

    Initial Commit and File Structure
    This is just the initial commit with some temporary file structure
    in place

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name="pyper-sketch",
+    version="0.0.1",
+    author="ErikWDev",
+    author_email="erikwdev@gmail.com",
+    description="A blank paper for your creative adventures in Python!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Canvim/Pyper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    license="MIT"
+)
