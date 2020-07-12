9ad31db072144b36e143a5ee41bb727df9ecd047
EriKWDev Josefins Dator <ErikWDev@gmail.com>
2020-07-12 19:25:09 +0200

    Added Josefin Example

--- pyperlib/__main__.py
+++ pyperlib/__main__.py
@@ -3,7 +3,7 @@ import argparse
 def cli():
     argument_parser = argparse.ArgumentParser(description="The Pyper CLI for managing Pyper Sketches", prog="pyperlib")
 
-    argument_parser.add_argument("-f", "--foo", help="Placeholder Argument!")
+    argument_parser.add_argument("-f", "--foo", help="Placeholder Josefin!")
 
     args = argument_parser.parse_args()
     main(args)
