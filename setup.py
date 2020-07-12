8b0eaedb231aa1db5257c117aed0be6c5be68ca1
EriKWDev Josefins Dator <ErikWDev@gmail.com>
2020-07-12 19:19:27 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-from pyperlib import __version__
-
 import setuptools
-
-with open("README.md", "r") as readme_file:
-    long_description = readme_file.read()
 
 setuptools.setup(
     setup_requires=["pbr"],
