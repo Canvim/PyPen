664cfac2c703c2cd7a20109fa3b3ce8f26da4613
EriKWDev Josefins Dator <ErikWDev@gmail.com>
2020-07-12 18:46:45 +0200

    Preparing for Travis and pbr setup

--- setup.py
+++ setup.py
@@ -6,24 +6,6 @@ with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
 setuptools.setup(
-    name="pyperlib",
-    version=__version__,
-    author="ErikWDev",
-    author_email="erikwdev@gmail.com",
-    description="A blank paper for your creative adventures in Python!",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Canvim/Pyper",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "moderngl",
-        "moderngl-window"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-    license="MIT"
+    setup_requires=["pbr"],
+    pbr=True
 )
