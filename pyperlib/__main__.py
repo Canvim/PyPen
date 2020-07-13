62435d48730c333923ad18f380fcad396c8390e9
ErikWDev <josefin@LAPTOP-NFLKM88R.localdomain>
2020-07-13 10:21:21 +0200

    WSL commit test

--- pyperlib/__main__.py
+++ pyperlib/__main__.py
@@ -1,14 +1,23 @@
 import argparse
 
 def cli():
-    argument_parser = argparse.ArgumentParser(description="The Pyper CLI for managing Pyper Sketches", prog="pyperlib")
+    try:
+        argument_parser = argparse.ArgumentParser(description="The Pyper CLI for managing Pyper Sketches", prog="pyper")
 
-    argument_parser.add_argument("-f", "--foo", help="Placeholder Josefin!")
+        argument_parser.add_argument("-f", "--fullscreen")
 
-    args = argument_parser.parse_args()
-    main(args)
+        arguments = argument_parser.parse_args()
 
-def main(args):
+        if len(sys.argv)==1:
+            argument_parser.print_help(sys.stderr)
+            sys.exit(1)
+
+        main(arguments)
+    except argparse.ArgumentError as error:
+        print(str(error))
+        sys.exit(2)
+
+def main(arguments):
     pass
 
 if __name__ == "__main__":
