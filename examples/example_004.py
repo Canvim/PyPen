a5b9530b25354de23edb7a2ba4f79dec984d554e
EriKWDev Josefins Dator <ErikWDev@gmail.com>
2020-07-14 17:03:23 +0200

    Added fill_screen and fill

---
+++ examples/example_004.py
@@ -0,0 +1,10 @@
+from pyperlib import *
+
+
+def start():
+    print("Start from example 4!")
+
+
+def update():
+    fill(colors.dark_green)
+    print("Update from example 4!")
--- pyperlib/drawing/primitives.py
+++ pyperlib/drawing/primitives.py
@@ -4,6 +4,12 @@ from pyperlib.drawing.color import colors
 def clear_screen():
     context.clear(colors.default_background_color.rgba())
 
+def fill_screen(color=colors.default_background_color):
+    context.clear(color=color.rgba())
+
+def fill(color=colors.default_background_color):
+    fill_screen(color=color)
+
 def rectangle(color=colors.default_color):
     context.clear(color=color.rgba())
     print("I am a pyper rectangle!")
