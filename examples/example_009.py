4e3b86640a69ba84d65bc9f194165849cc0b698a
Erik Gren <erigr@axis.com>
2020-08-03 09:12:01 +0200

    Added Accidentally Removed Example Code

--- examples/example_009.py
+++ examples/example_009.py
@@ -8,6 +8,22 @@ def start():
 def update():
     fill_screen("#343434")
 
+    begin_shape()
+    vertex(40, 40)
+    vertex(40, 140)
+    vertex(FRAME, 140)
+    end_shape("red")
+
+    begin_shape()
+    vertex(40+FRAME, 240+FRAME)
+    vertex(40, 340)
+    vertex(FRAME, 340)
+    vertex(340, 280)
+
+    end_shape("yellow", "blue", 5)
+
+    reset_style()
+
     angle = 0
     points = 10 + sin(TIME*2)*5
     radius = 100
@@ -23,3 +39,4 @@ def update():
 
     end_shape("orange")
     restore()
+    reset_style()
